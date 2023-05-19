da3972fd969ce621100dae1069994d4e5e43a2dc
Shailesh.G <Shailesh.G@A4-G-708-WINDOWS.afourtech.pune>
2023-05-19 20:04:57 +0530

    Completed Header UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,37 @@
 import { useDispatch } from "react-redux";
 import { toggleSidebar } from "../../../store/slices/uiSlice";
+import { BsSearch } from "react-icons/bs";
+import { BiUserCircle } from "react-icons/bi";
+import { CiMenuFries } from "react-icons/ci";
+import Logo from "../Logo/Logo";
 import "./Header.scss";
 
 function Header() {
   const dispatch = useDispatch();
 
   return (
-    <div className="header-wrapper">
-      Header
-      <button onClick={() => dispatch(toggleSidebar())}>click</button>
-    </div>
+    <header className="header-wrapper">
+      <div className="header-logo">
+        <Logo />
+      </div>
+      <div className="search-wrapper">
+        <div className="search-container">
+          <input className="search" placeholder="Search" type="text" />
+          <button className="search-btn">
+            <BsSearch />
+          </button>
+        </div>
+        <div className="cta-section">
+          <a className="cta-btn" onClick={() => dispatch(toggleSidebar())}>
+            <BiUserCircle />
+            Sign In
+          </a>
+        </div>
+      </div>
+      <div className="menu-btn" onClick={() => dispatch(toggleSidebar())}>
+        <CiMenuFries />
+      </div>
+    </header>
   );
 }
 
