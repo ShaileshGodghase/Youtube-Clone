6952eac7935f4d1ea40afcbefe74a7b6af2ee1ef
Shailesh.G <Shailesh.G@A4-G-708-WINDOWS.afourtech.pune>
2023-05-19 12:43:58 +0530

    Added client side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
-import './globals.scss';
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import "./globals.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import { Suspense } from "react";
+import Loader from "./components/Loader/Loader";
 
 function App() {
-
   return (
     <>
-    <Provider store={store}>
-    <h1>Youtube Clone</h1>
-
-    </Provider>
+      <Provider store={store}>
+        <Suspense fallback={<Loader />}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
